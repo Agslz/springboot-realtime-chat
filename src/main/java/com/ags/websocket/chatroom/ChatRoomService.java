8a43da848661b57d805c6611b274d985bb838f93
Agustín Schulze <agsp3rs3o@gmail.com>
2024-01-17 11:57:36 -0300

    Fix implement getChatRoomId

--- src/main/java/com/ags/websocket/chatroom/ChatRoomService.java
+++ src/main/java/com/ags/websocket/chatroom/ChatRoomService.java
@@ -14,7 +14,22 @@ public class ChatRoomService {
     public Optional<String> getChatRoomId(
             String senderId, String recipientId, boolean createNewRoomIfNotExists
     ) {
-        return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
+        return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId).map(ChatRoom::getChatId).or(() -> {
+            if (createNewRoomIfNotExists) {
+                var chatId = createChatId(senderId, recipientId);
+                return Optional.of(chatId);
+            }
+            return Optional.empty();
+        });
+    }
+
+    private String createChatId(String senderId, String recipientId) {
+        var chatId = String.format(("%s_%s"), senderId, recipientId); //agustin_julian
+        ChatRoom senderRecipient = ChatRoom.builder().chatId(chatId).senderId(senderId).recipientId(recipientId).build();
+        ChatRoom recipientSender = ChatRoom.builder().chatId(chatId).senderId(recipientId).recipientId(senderId).build();
+        chatRoomRepository.save(senderRecipient);
+        chatRoomRepository.save(recipientSender);
+        return chatId;
     }
 
 
